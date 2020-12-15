4fc0cc7bc74a795b5d8db6ee8e1df8281d9906d1
Jesus Gonzalez <jagltoro@gmail.com>
2020-12-14 23:14:46 -0300

    Changed Touchable for Pressable Inverted the tags for pressable and the container box for easier press Added optional actions for press

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,22 +3,26 @@ import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesome5 as Icon } from "@expo/vector-icons";
 
 import { Box, Text, Theme, useTheme } from "./Theme";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { Pressable } from "react-native";
 
 interface HeaderProps {
-  paddingVertical: keyof Theme['spacing'];
+  paddingVertical: keyof Theme["spacing"];
   title: string;
+  actions?: {
+    left?: () => void;
+    right?: () => void;
+  };
   icons?: {
     left?: string;
     right?: string;
   };
 }
 
-const Header = ({paddingVertical, title, icons}: HeaderProps) => {
+const Header = ({ paddingVertical, title, icons, actions }: HeaderProps) => {
   const theme = useTheme();
   return (
     <LinearGradient
-      colors={[theme.colors['gradientStart'], theme.colors['gradientEnd']]}
+      colors={[theme.colors["gradientStart"], theme.colors["gradientEnd"]]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 0 }}
     >
@@ -28,30 +32,41 @@ const Header = ({paddingVertical, title, icons}: HeaderProps) => {
         zIndex={10}
         justifyContent="center"
         alignItems="center"
-        {...{paddingVertical}}
+        {...{ paddingVertical }}
       >
-        {
-          (icons && icons.left) &&
-          <Box style={{ position: "absolute", left: theme.spacing.m }}>
-            <TouchableWithoutFeedback>
+        {icons && icons.left && (
+          <Pressable onPress={actions?.left} style={{ position: "absolute", left: theme.spacing.m }}>
+          <Box
+            style={{ left: -theme.spacing.m }}
+            padding="m"
+          >
               <Icon
                 name={icons.left}
                 size={20}
                 color={theme.colors["headerText"]}
               />
-            </TouchableWithoutFeedback>
           </Box>
-        }
+            </Pressable>
+        )}
 
         <Text variant="header">{title}</Text>
-        {
-          (icons && icons.right) &&
-          <Box style={{ position: "absolute", right: theme.spacing.m }}>
-            <TouchableWithoutFeedback>
-              <Icon name={icons.right} size={20} color={theme.colors["headerText"]} />
-            </TouchableWithoutFeedback>
-          </Box>
-        }
+        {icons && icons.right && (
+          <Pressable
+            onPress={actions?.right}
+            style={{ position: "absolute", right: theme.spacing.m }}
+          >
+            <Box
+              style={{ right: -theme.spacing.m }}
+              padding="m"
+            >
+              <Icon
+                name={icons.right}
+                size={20}
+                color={theme.colors["headerText"]}
+              />
+            </Box>
+          </Pressable>
+        )}
       </Box>
     </LinearGradient>
   );
